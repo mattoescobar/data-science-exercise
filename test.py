bffdcec1e2d26f1634118cbbcdaf31ed57744744
Matt Escobar <mattoescobar@gmail.com>
2018-10-03 19:23:31 +0100

    labeled test data prediction

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pickle
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 # TEST DATA PREDICTION - Part 3
 #
@@ -10,5 +12,37 @@ import pickle
 regressor = pickle.load(open("xgboost.dat", "rb"))
 data = np.load('data_xgboost.npz')
 test_data = data['arr_3']
-X_test_labeled = test_data[:, 1:-1]
+X_test_labeled = test_data[:, 1:]
 y_test = test_data[:, 0].reshape(-1, 1)
+
+# Labeled test data prediction - It assumes that after each prediction measurements indicating the actual values
+# of variables is available
+training_data = data['arr_2']
+id_arr = data['arr_4']
+len_household = int(training_data.shape[0] / len(id_arr))
+len_household_test = int(y_test.shape[0] / len(id_arr))
+y_pred_test = []
+for i in range(len(id_arr)):
+    X_last_train = training_data[(i + 1) * len_household - 1, 1:].reshape(-1, 1)
+    y_last_train = training_data[(i + 1) * len_household - 1, 0]
+    new_x = np.array([y_last_train, y_last_train - X_last_train[0][0]]).reshape(-1, 1)
+    X_test = np.concatenate((new_x, X_last_train[:-2]))
+    X_test = np.reshape(X_test, (1, X_test.shape[0]))
+    for j in range(len_household_test):
+        y_regressor = regressor.predict(X_test)
+        y_pred_test.append(y_regressor[0])
+        y_real = y_test[i * len_household_test + j]
+        new_x = np.reshape(np.array([y_real, y_real - X_test[0][0]]), (1, 2))
+        X_test = np.concatenate((new_x, X_test[:, :-2]), axis=1)
+y_pred_test = np.array(y_pred_test).reshape(-1, 1)
+mse_test = mean_squared_error(y_test, y_pred_test)
+
+for i in range(len(id_arr)):
+    plt.plot(y_test[len_household_test*i:len_household_test*(i+1), 0], color='red', label=[id_arr[i] + ' actual values'])
+    plt.plot(y_pred_test[len_household_test*i:len_household_test*(i+1), 0], color='blue', label=[id_arr[i] +
+             ' predicted values'])
+    plt.title('Usage for the ' + id_arr[i] + ' household')
+    plt.xlabel('Time')
+    plt.ylabel('Usage')
+    plt.legend()
+    plt.show()
--- train.py
+++ train.py
@@ -10,7 +10,6 @@ def feature_engineering(dataset, time_delay=1):
     # Split data per household
     usage_household = dataset.pivot(index='datetime', columns='id', values='usage')
     usage_delayed = usage_household
-    usage_diff = usage_household - usage_household.shift(1)
     # Creating features per household
     for i in range(time_delay):
         # Previous usage as feature
@@ -20,7 +19,6 @@ def feature_engineering(dataset, time_delay=1):
         usage_delayed = usage_delayed.join(usage_iter, rsuffix=('_'+str(i+1)))
         usage_delayed = usage_delayed.join(usage_diff_iter, rsuffix=('_diff'+str(i+1)))
     usage_household = usage_delayed
-    usage_household = usage_household.join(usage_diff, rsuffix='diff')
     # Clearing nan values due to time delay
     usage_household = usage_household.iloc[1+time_delay:, :]
 
@@ -40,7 +38,7 @@ def feature_engineering(dataset, time_delay=1):
         train_dataset = pd.concat([train_dataset, df])
         test_dataset = pd.concat([test_dataset, df_test])
 
-    for i in range(2*time_delay + 1):  # Combining features
+    for i in range(2*time_delay):  # Combining features
         df_iter = usage_household.iloc[:, (i+1)*households]
         df_iter_test = usage_test_labeled.iloc[:, (i + 1) * households]
         for j in range(households-1):
@@ -59,7 +57,7 @@ dataset_train = pd.read_csv('usage_train.csv')
 
 # Feature engineering with time delay = 48 (one day, roughly two usage cycles)
 usage_household, usage_test_labeled, train_dataset, test_dataset = feature_engineering(dataset_train)
-X_train = train_dataset.iloc[:, 1:-1].values
+X_train = train_dataset.iloc[:, 1:].values
 y_train = train_dataset.iloc[:, 0].values.reshape(-1, 1)
 
 # BUILDING THE MODEL - Part 2
